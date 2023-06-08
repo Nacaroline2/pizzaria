2f70beeda391a73b0ab38c87ee7d4ac28d9a5a94
Nacaroline2 <anacarolinesiqueira2@gmail.com>
2023-06-07 22:12:51 -0300

    commit App java

--- aplicacao/App.java
+++ aplicacao/App.java
@@ -1,5 +1,18 @@
 package aplicacao;
 
+import Model.Pizza;
+
 public class App {
+    public static void main(String[] args) {
+        Pizza p = new Pizza("Frango", 30.0, 'p');
+
+        System.out.println("Pedido realizado!!");
+        System.out.println(p.pedidoIfood());
+
+        Pizza p2 = new Pizza("camarao", 50.0, 'G');
+
+        System.out.println("Pedido Criado!!");
+        System.out.println(p2.pedidoIfood());
+    }
     
 }
